a8c4eedb54ed2d421c79e71273cd975900f607c8
Raul Alcantara <ralcanta@mit.edu>
2019-08-13 11:35:34 -0400

    All info from the database is here and its changes will be made here

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet,Image, Text, View, Animated } from 'react-native';
+import Presentation from "./components/Presentation.js"
+import Swipeable from 'react-native-swipeable';
+
+
+//IN CASE "expo start" does not connect with the correct IP address of the computer
+// export env REACT_NATIVE_PACKAGER_HOSTNAME=192.168.41.89
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      energy:3.5,
+      mood: 6.1,
+      curiosity: 8,
+
+      is_first: false,
+    }
+  }
+  updateAttribute = (type) =>{
+    //TODO: Update it from the database!
+    switch(type){
+      case "energy":{
+        this.setState({
+          energy: this.state.energy+1
+        })
+        break;
+      }
+      case "mood":{
+        this.setState({
+          mood: this.state.mood+1
+        })
+        break;
+      }
+      case "curiosity":{
+        this.setState({
+          curiosity: this.state.curiosity+1
+        })
+        break;
+      }
+    }
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Presentation
+        values ={this.state}
+        updateAttribute={this.updateAttribute}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FF6865',
+  },
+});
