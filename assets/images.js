aa06746d34b3549ee3926afb41c56cb3d14a71d0
Raul Alcantara <ralcanta@mit.edu>
2019-08-13 11:36:49 -0400

    all text images stored in images.js

---
+++ assets/images.js
@@ -0,0 +1,111 @@
+const images = {
+    "energy":{
+        "0":{
+            text: require('./img/energy_texts/text_energy_0.png')
+        },
+        "1": {
+            text: require('./img/energy_texts/text_energy_1.png')
+        },
+        "2": {
+            text: require('./img/energy_texts/text_energy_2.png')
+        },
+        "3": {
+            text: require('./img/energy_texts/text_energy_3.png')
+        },
+        "4": {
+            text: require('./img/energy_texts/text_energy_4.png')
+        },
+        "5": {
+            text: require('./img/energy_texts/text_energy_5.png')
+        },
+        "6": {
+            text: require('./img/energy_texts/text_energy_6.png')
+        },
+        "7": {
+            text: require('./img/energy_texts/text_energy_7.png')
+        },
+        "8": {
+            text: require('./img/energy_texts/text_energy_8.png')
+        },
+        "9": {
+            text: require('./img/energy_texts/text_energy_9.png')
+        },
+        "10": {
+            text: require('./img/energy_texts/text_energy_10.png')
+        },
+   
+    },
+    "mood":{
+        "0": {
+            text: require('./img/mood_texts/text_mood_0.png')
+        },
+        "1": {
+            text: require('./img/mood_texts/text_mood_1.png')
+        },
+        "2": {
+            text: require('./img/mood_texts/text_mood_2.png')
+        },
+        "3": {
+            text: require('./img/mood_texts/text_mood_3.png')
+        },
+        "4": {
+            text: require('./img/mood_texts/text_mood_4.png')
+        },
+        "5": {
+            text: require('./img/mood_texts/text_mood_5.png')
+        },
+        "6": {
+            text: require('./img/mood_texts/text_mood_6.png')
+        },
+        "7": {
+            text: require('./img/mood_texts/text_mood_7.png')
+        },
+        "8": {
+            text: require('./img/mood_texts/text_mood_8.png')
+        },
+        "9": {
+            text: require('./img/mood_texts/text_mood_9.png')
+        },
+        "10": {
+            text: require('./img/mood_texts/text_mood_10.png')
+        },
+ 
+    },
+    "curiosity":{
+        "0": {
+            text: require('./img/curiosity_texts/text_curiosity_0.png')
+        },
+        "1": {
+            text: require('./img/curiosity_texts/text_curiosity_1.png')
+        },
+        "2": {
+            text: require('./img/curiosity_texts/text_curiosity_2.png')
+        }, 
+        "3": {
+            text: require('./img/curiosity_texts/text_curiosity_3.png')
+        }, 
+        "4": {
+            text: require('./img/curiosity_texts/text_curiosity_4.png')
+        }, 
+        "5": {
+            text: require('./img/curiosity_texts/text_curiosity_5.png')
+        }, 
+        "6": {
+            text: require('./img/curiosity_texts/text_curiosity_6.png')
+        }, 
+        "7": {
+            text: require('./img/curiosity_texts/text_curiosity_7.png')
+        }, 
+        "8": {
+            text: require('./img/curiosity_texts/text_curiosity_8.png')
+        }, 
+        "9": {
+            text: require('./img/curiosity_texts/text_curiosity_9.png')
+        }, 
+        "10": {
+            text: require('./img/curiosity_texts/text_curiosity_10.png')
+        }, 
+    }
+
+}
+export default images;
